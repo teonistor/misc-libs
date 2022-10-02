3f8880e3af4f820134b20b9ce132db60ec3ca62d
Teodor G Nistor <teodor@nistor.uk>
2022-10-02 20:15:10 +0100

    Sweep some WIP under the rug

--- poor-mans-pipeline/src/test/scala/io/github/teonistor/poormanspipeline/PipelineCreatorTest.scala
+++ poor-mans-pipeline/src/test/scala/io/github/teonistor/poormanspipeline/PipelineCreatorTest.scala
@@ -2,8 +2,4 @@ package io.github.teonistor.poormanspipeline
 
 class PipelineCreatorTest {
 
-  @Test
-  def bob(): Unit ={
-    println(new PipelineCreator().bob())
-  }
 }
--- stream-utils/src/test/scala/io/github/teonistor/unusefullib/streaming/LazyGroupingLazyListTest.scala
+++ stream-utils/src/test/scala/io/github/teonistor/unusefullib/streaming/LazyGroupingLazyListTest.scala
@@ -1,12 +1,11 @@
 package io.github.teonistor.unusefullib.streaming
 
-import io.github.teonistor.unusefullib.streaming.LazyGroupingLazyList.LazyListHasLazyGroupBy
 import org.scalatest.FunSuite
 
 class LazyGroupingLazyListTest extends FunSuite {
 
-  test("Aaa") {
+  /*test("Aaa") {
 
     LazyList.from(1).lazyGroupBy(_=>"")
-  }
+  }*/
 }
