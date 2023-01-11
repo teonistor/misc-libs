89bbded59b038d15f9595d3ca5d53334816bd7bc
Teodor G Nistor <teodor@nistor.uk>
2023-01-11 21:09:55 +0000

    Runner

--- git-bang/src/main/scala/io/github/teonistor/gitbang/Runner.scala
+++ git-bang/src/main/scala/io/github/teonistor/gitbang/Runner.scala
@@ -1,6 +1,24 @@
 package io.github.teonistor.gitbang
 
-class Runner {
+import java.io.{BufferedReader, File, InputStreamReader}
+import java.lang.ProcessBuilder.Redirect.INHERIT
+import scala.jdk.CollectionConverters.IteratorHasAsScala
+import scala.util.Using
 
-  def run(command: String*):String = ???
+class Runner (dir: File) {
+
+  def run(command: String*):String = {
+    val process = new ProcessBuilder(command: _*)
+      .directory(dir)
+      .redirectInput(INHERIT)
+      .redirectError(INHERIT)
+      .start()
+
+    val output = Using(new BufferedReader(new InputStreamReader(process.getInputStream)))(_
+      .lines().iterator().asScala.mkString("\n"))
+
+    println(output)
+    println("Exit " + process.waitFor())
+    output.get
+  }
 }
