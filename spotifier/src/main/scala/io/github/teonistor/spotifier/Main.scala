0452a33c5985cc08c7b9f1f3bd9c130289a9c304
Teodor G Nistor <teodor@nistor.uk>
2025-09-21 17:39:08 +0100

    TIX-1002 Update Spotifier entry point

--- spotifier/src/main/scala/io/github/teonistor/spotifier/Main.scala
+++ spotifier/src/main/scala/io/github/teonistor/spotifier/Main.scala
@@ -4,10 +4,14 @@ object Main {
   def main(arg: Array[String]): Unit = {
 //    Coordinator.createDirectoiesAndFiles()
     Coordinator.snapshotAccount()
+
     Coordinator.historiciseByLatestName("Board_Games")
-//    Coordinator.historiciseByLatestName("Carabin_teonoo7")
     Coordinator.historiciseByLatestName("Carabin_Nicolae_teonoo7")
+    Coordinator.historiciseByLatestName("Rom_ne_ti")
+    Coordinator.historiciseByLatestName("On_Repeat")
+    Coordinator.historiciseByLatestName("Foc_mocnit_de_tab_r_")
+    Coordinator.historiciseByLatestName("Mezzo_Piano")
+    Coordinator.historiciseByLatestName("Mezzo_Forte")
     Coordinator.historiciseByLatestName("Repeat_Rewind")
-    Coordinator.historiciseByLatestName("Your_Top_Songs_2024")
   }
 }
